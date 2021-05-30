001fb6a9081944a8acfa21eceb5b888d1eb9b1fa
MinnuBaby <84379531+MinnuBaby@users.noreply.github.com>
2021-05-30 22:31:11 +0530

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,38 +1,52 @@
 package course_api.springbootstarter.item;
+import lombok.NonNull;
+import lombok.Getter;
+import lombok.Setter;
+
 
 public class Item implements Comparable<Item>
 {
-	private String id;
-	private String brand;
-	private String description;
-	private int price;
-
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getBrand() {
-		return brand;
-	}
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
 	
-	public Item(String id, String brand, String description, int price) {
+	private String id;
+	@Getter@Setter
+	private String brand;
+	@Getter@Setter
+	private String description;
+	@Getter@Setter
+	private int price;
+	
+	
+	 public String getId() 
+	 {
+		 return id;
+	 } 
+	 public void setId(String id) 
+	 { 
+		 this.id=id; 
+	 }
+	 
+	
+	  public String getBrand()
+	  { return brand; }
+	  public void setBrand(String brand)
+	  { this.brand = brand; }
+	  public String getDescription()
+	  { return description; }
+	  public void setDescription(String description) 
+	  { this.description = description; }
+	 
+	
+	  public int getPrice()
+	  {
+		  return price;
+	  }
+	  public void setPrice(int price) 
+	  {
+		  this.price = price; 
+	  }
+	 
+	
+	public Item(@NonNull String id, String brand, String description, int price) {
 		super();
 		this.id = id;
 		this.brand = brand;
