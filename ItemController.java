a5b3ef74c1a4866ac0572d34996d493141464bfd
MinnuBaby <84379531+MinnuBaby@users.noreply.github.com>
2021-05-30 22:29:40 +0530

    Update ItemController.java

--- ItemController.java
+++ ItemController.java
@@ -1,6 +1,6 @@
 package course_api.springbootstarter.item;
-import java.util.Arrays;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,60 +8,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class ItemController 
 {
 	@Autowired
 	private BusinessService business_service; // creating an instance to business class . all things can be written in business class
 	
-	@RequestMapping("/getAll")
+	@RequestMapping("/item-details/items/")
 	public List<Item> getallitems() 
 	{
 		return business_service.getallitems();
 	}
-	@RequestMapping("/getAll2")
-	public List<Item> getallitems2() 
-	{
-		return Arrays.asList
-				(
-						new Item("Item1","UV MARKER","desc1",50),
-						new Item("Item2","XYZ12345AS","desc2",100),
-						new Item("Item3","EFG34567BF","desc3",150),
-						new Item("Item4","XYZ135788S","desc4",200),
-						new Item("Item5","TOMBOW","desc5",250),
-						new Item("Item6","WATERMAN","desc6",300),
-						new Item("Item7","ESSELTE","desc7",350),
-						new Item("Item8","DUNHILL","desc8",400),
-						new Item("Item9","DGH3488S","desc9",450),
-						new Item("Item10","CAMLIN","desc10",500),
-						new Item("Item11","WSX234RTY","desc11",550),
-						new Item("Item12","ANOTO","desc12",600),
-						new Item("Item13","VHGFYRUI","desc13",650),
-						new Item("Item14","CARTIER","desc14",700),
-						new Item("Item15","OPDWTYY","desc15",750),
-						new Item("Item16","KIROF234","desc16",800),
-						new Item("Item17","SQWEE123","desc17",850),
-						new Item("Item18","SRTYUUIO","desc18",900),
-						new Item("Item19","KASLLSW","desc19",950),
-						new Item("Item20","KASERRW","desc20",120)	
-				);
-	}
-	@RequestMapping("/getAll/{id}")
+	
+	@RequestMapping("/item-details/items/{id}")
 	public Item GetSingleItem(@PathVariable String id)
 	{
 		return business_service.get_item(id);
 	}
-	@RequestMapping(method = RequestMethod.POST,value = "/setItem")
+	@RequestMapping(method = RequestMethod.POST,value = "/item-details/items/")
 	public void addItem(@RequestBody Item item)
 	{
 		business_service.addItem(item);
 	}
-	@RequestMapping(method = RequestMethod.PUT,value = "/updateItem/{id}")
+	@RequestMapping(method = RequestMethod.PUT,value = "/item-details/items/{id}")
 	public void UpdateItem(@RequestBody Item item,@PathVariable String id)
 	{
 		business_service.UpdateItem(item,id);
 	}
-	@RequestMapping(method = RequestMethod.DELETE,value = "/deleteItem/{id}")
+	@RequestMapping(method = RequestMethod.DELETE,value = "/item-details/items/{id}")
 	public void DeleteItem(@PathVariable String id)
 	{
 		business_service.deleteItem(id);
